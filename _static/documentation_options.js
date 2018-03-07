71896b8c18fcf3f260407f5460ff61385cbd36c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-07 00:50:19 +0000

    Deploy sih4sing5hong5/tai5-uan5_gian5-gi2_kang1-ku7 to github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_kang1-ku7.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.6.32',
+    LANGUAGE: 'zh_tw',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
